f02d4dc0a45bef174bc812baa01154fa6491fce8
aabb <16666666666@163.com>
2025-11-06 22:42:05 +0800

    chore(ServiceContext): add  ArticleModel

--- internal/svc/servicecontext.go
+++ internal/svc/servicecontext.go
@@ -11,15 +11,17 @@ import (
 )
 
 type ServiceContext struct {
-	Config     config.Config
-	FocusModel mysql.FocusModel // 轮播图模型
+	Config       config.Config
+	FocusModel   mysql.FocusModel // 轮播图模型
+	ArticleModel mysql.ArticleModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	// 初始化数据库
 	slqx := sqlx.NewMysql(c.Mysql.DataSource) // 新建数据库连接
 	return &ServiceContext{
-		Config:     c,
-		FocusModel: mysql.NewFocusModel(slqx),
+		Config:       c,
+		FocusModel:   mysql.NewFocusModel(slqx),
+		ArticleModel: mysql.NewArticleModel(slqx),
 	}
 }
